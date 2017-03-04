6a90a4b5876bba9aa6e1bde5f23b84bbe718fdfd
Jason Mayo <jason@blottostudio.com>
2017-03-04 11:18:34 +0000

    blt settings

--- farmodo/settings.py
+++ farmodo/settings.py
@@ -4,3 +4,8 @@
 other setup data for farmodo """
 
 pathToDB = "/Volumes/blotto_studio/blottoweb/database/blottoTestFarmDB.db"
+tagFileName = '/tmp/blt_lockFile'
+contentPath = '/Volumes/blotto_studio/RenderFarm/modo/Content'
+linuxModo = '/Volumes/blotto_studio/RenderFarm/modo/app/modo/modo_cl'
+OSXModo = '/Volumes/blotto_studio/RenderFarm/modo/macOs_app/modo.app/Contents/MacOS/modo_cl'
+renderLog = '/tmp/modo_render_log'
